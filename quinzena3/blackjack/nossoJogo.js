ea266fa3a625918e4bc5e701e80e49947dcf9807
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-03 01:57:45 -0300

    Dinãmica do jogo feita

--- quinzena3/blackjack/nossoJogo.js
+++ quinzena3/blackjack/nossoJogo.js
@@ -10,3 +10,30 @@
  * 
  * 
  */
+
+console.log("Bem vindo(a) ao jogo de Blackjack!");
+
+let iniciarJogo = confirm("Quer iniciar uma nova rodada?");
+
+if (iniciarJogo) {
+   let cartasUsuario = [comprarCarta(), comprarCarta()];
+   let pontuacaoUsuario = Number(cartasUsuario[0].valor + cartasUsuario[1].valor);
+   
+   let cartasComputador = [comprarCarta(), comprarCarta()];
+   let pontuacaoComputador = Number(cartasComputador[0].valor + cartasComputador[1].valor);
+
+   console.log("Usuário - cartas: " + cartasUsuario[0].texto + " " + cartasUsuario[1].texto + " - pontuação " + pontuacaoUsuario);
+
+   console.log("Computador - cartas: " + cartasComputador[0].texto + " " + cartasComputador[1].texto + " - pontuação " + pontuacaoComputador);
+
+   if (pontuacaoUsuario > pontuacaoComputador){
+      console.log("O usuário ganhou!");
+   } else if (pontuacaoUsuario < pontuacaoComputador) {
+      console.log("O computador ganhou!");
+   } else {
+      console.log("Empate!");
+   }
+
+} else {
+   console.log("O jogo acabou.")
+}
