7114e61806b8faaa37e596387f9582a2b191106b
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-03 19:21:19 -0300

    Lógica da compra de novas cartas

--- quinzena3/blackjack/desafio.js
+++ quinzena3/blackjack/desafio.js
@@ -12,18 +12,31 @@
  */
 
 let cartasUsuario = [comprarCarta(), comprarCarta()];
+let pontuacaoUsuario = Number(cartasUsuario[0].valor + cartasUsuario[1].valor);
+
 let cartasComputador = [comprarCarta(), comprarCarta()];
+let pontuacaoComputador = Number(cartasComputador[0].valor + cartasComputador[1].valor);
 
-console.log(cartasUsuario[0].texto.slice(0, 1))
-console.log(cartasUsuario[1].texto.slice(0, 1))
-console.log(cartasComputador[0].texto.slice(0, 1))
-console.log(cartasComputador[1].texto.slice(0, 1))
+let verificaAA = Boolean(cartasUsuario[0].texto.slice(0, 1) == "A" && cartasUsuario[1].texto.slice(0, 1) == "A") || Boolean(cartasComputador[0].texto.slice(0, 1) == "A" && cartasComputador[1].texto.slice(0, 1) == "A");
 
-verificaAA = Boolean(cartasUsuario[0].texto.slice(0, 1) == "A" && cartasUsuario[1].texto.slice(0, 1) == "A") || Boolean(cartasComputador[0].texto.slice(0, 1) == "A" && cartasComputador[1].texto.slice(0, 1) == "A");
-
-console.log(verificaAA);
+console.log(verificaAA)
 
 if (verificaAA){
    cartasUsuario = [comprarCarta(), comprarCarta()];
    cartasComputador = [comprarCarta(), comprarCarta()];
 }
+
+let comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
+
+if (comprarOutra) {
+   cartasUsuario.push(comprarCarta());
+   pontuacaoUsuario = Number(pontuacaoUsuario + cartasUsuario[2].valor)
+   if (pontuacaoUsuario <= 21) {
+      comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto}, ${cartasUsuario[1].texto} e  ${cartasUsuario[2].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
+   } else {
+      alert(`Você perdeu! sua pontuação de ${pontuacaoUsuario} extrapolou 21.`)  
+   }
+
+} 
+
+console.log(cartasUsuario)
