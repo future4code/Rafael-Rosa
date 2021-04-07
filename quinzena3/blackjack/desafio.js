caef808d92b206f92cb3e1f15291b8d665a777ee
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-07 00:16:32 -0300

    desafio 8 feito - realizando o 9

--- quinzena3/blackjack/desafio.js
+++ quinzena3/blackjack/desafio.js
@@ -17,26 +17,79 @@ let pontuacaoUsuario = Number(cartasUsuario[0].valor + cartasUsuario[1].valor);
 let cartasComputador = [comprarCarta(), comprarCarta()];
 let pontuacaoComputador = Number(cartasComputador[0].valor + cartasComputador[1].valor);
 
-let verificaAA = Boolean(cartasUsuario[0].texto.slice(0, 1) == "A" && cartasUsuario[1].texto.slice(0, 1) == "A") || Boolean(cartasComputador[0].texto.slice(0, 1) == "A" && cartasComputador[1].texto.slice(0, 1) == "A");
+let verificaAA = Boolean((pontuacaoUsuario === 22) || (pontuacaoComputador === 22));
 
-console.log(verificaAA)
-   
-if (verificaAA){
-   cartasUsuario = [comprarCarta(), comprarCarta()];
-   cartasComputador = [comprarCarta(), comprarCarta()];
+if (verificaAA) {
+   do {
+      cartasUsuario = [comprarCarta(), comprarCarta()];
+      pontuacaoUsuario = Number(cartasUsuario[0].valor + cartasUsuario[1].valor);
+
+      cartasComputador = [comprarCarta(), comprarCarta()];
+      pontuacaoComputador = Number(cartasComputador[0].valor + cartasComputador[1].valor);
+
+      verificaAA = Boolean((pontuacaoUsuario === 22) || (pontuacaoComputador === 22));
+   } while (verificaAA);
 }
 
-let comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
+console.log(cartasUsuario);
+console.log(pontuacaoUsuario);
+console.log("teste");
 
-if (comprarOutra) {
-   cartasUsuario.push(comprarCarta());
-   pontuacaoUsuario = Number(pontuacaoUsuario + cartasUsuario[2].valor)
-   if (pontuacaoUsuario <= 21) {
-      comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto}, ${cartasUsuario[1].texto} e  ${cartasUsuario[2].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
-   } else {
-      alert(`Você perdeu! sua pontuação de ${pontuacaoUsuario} extrapolou 21.`)  
+
+let textoCartas 
+let comprarNovaCarta = confirm("Deseja comprar mais uma carta?");
+
+   if (comprarNovaCarta) {
+      NovaCarta(cartasUsuario);
    }
 
-} 
 
-console.log(cartasUsuario)
+console.log(cartasUsuario);
+console.log(pontuacaoUsuario);
+
+
+// do {
+//    let indice = 0
+//    let comprarNovaCarta = usuarioNovaCarta();
+
+//    let comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
+
+// } while ();
+
+function NovaCarta(jogador) {
+   jogador.push(comprarCarta())
+   atualizaPontuacao()
+   
+   return jogador;
+}
+
+function atualizaPontuacao(){
+   let ultimaPosicao = Number(cartasUsuario.length) - 1;
+   pontuacaoUsuario += cartasUsuario[ultimaPosicao].valor;
+}
+
+
+
+
+// let comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto} e ${cartasUsuario[1].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
+
+//  if (comprarOutra) {
+//     cartasUsuario.push(comprarCarta());
+//     pontuacaoUsuario = Number(pontuacaoUsuario + cartasUsuario[2].valor)
+//     if (pontuacaoUsuario <= 21) {
+//        comprarOutra = confirm(`Suas cartas são ${cartasUsuario[0].texto}, ${cartasUsuario[1].texto} e  ${cartasUsuario[2].texto}. A carta revelada do computador é ${cartasComputador[0].texto}.` + "\n" + "Deseja comprar mais uma carta?");
+//     } else {
+//        alert(`Você perdeu! sua pontuação de ${pontuacaoUsuario} extrapolou 21.`)
+//     }
+
+//  }
+
+//  console.log(cartasUsuario)
+
+
+
+
+
+//  function computadorNovaCarta() {
+//    cartasComputador.push(comprarCarta())
+// }
