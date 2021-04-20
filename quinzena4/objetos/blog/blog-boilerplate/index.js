4203c3865d72e8e8372a836013233d5aa62cb1aa
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-19 23:07:47 -0300

    logica do exercicio pronta

--- quinzena4/objetos/blog/blog-boilerplate/index.js
+++ quinzena4/objetos/blog/blog-boilerplate/index.js
@@ -1,24 +1,40 @@
 
+arrayTeste = [];
 
-function imprimir(){
-    const arrayTeste = [];
+function incluirPost() {
 
-    const entradasUsuario = {
-        titulo: document.getElementById('titulo-post').value,
-        autor: document.getElementById('autor-post').value,
-        conteudo: document.getElementById('conteudo-post').value
+    const tituloDoPost = document.getElementById('titulo-post').value
+    const autorDoPost = document.getElementById('autor-post').value
+    const conteudoDoPost = document.getElementById('conteudo-post').value
+
+    if (tituloDoPost.length > 0 && autorDoPost.length > 0 && conteudoDoPost.length > 0) {
+        postAdicionado = capturarEntradasUsuario(tituloDoPost, autorDoPost, conteudoDoPost) //retorna objeto
+        arrayTeste.push(postAdicionado);
+        limparInputs()
+        publicarPost()
+    } else {
+        alert('Por favor, preencha todos os campos');
+        return
     }
+    console.log(arrayTeste)
+}
 
-    arrayTeste.push(entradasUsuario);
+function publicarPost(){
+    let publicarPost = document.getElementById('container-de-posts')
+    publicarPost.innerHTML += `<div class="card-post">${arrayTeste[arrayTeste.length - 1].titulo}<br>${arrayTeste[arrayTeste.length - 1].autor}<br>${arrayTeste[arrayTeste.length - 1].conteudo}</div>`
+}
 
+function capturarEntradasUsuario(titulo, autor, conteudo) {
+    const post = {
+        titulo: titulo,
+        autor: autor,
+        conteudo: conteudo
+    }
+    return post
+}
+
+function limparInputs() {
     document.getElementById('titulo-post').value = ""
     document.getElementById('autor-post').value = ""
     document.getElementById('conteudo-post').value = ""
-
-    let publicarPost = document.getElementById('container-de-posts')
-    publicarPost.innerHTML = `<div class="card-post">${arrayTeste[0].titulo}<br>${arrayTeste[0].autor}<br>${arrayTeste[0].conteudo}</div>`
-
-
-    console.log(entradasUsuario)
-    console.log(arrayTeste)
 }
