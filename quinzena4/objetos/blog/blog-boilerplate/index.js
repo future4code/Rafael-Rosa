ecc313f9e5bdccfef285e00a59e4acd658b8f7b7
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-19 23:41:29 -0300

    melhorias css

--- quinzena4/objetos/blog/blog-boilerplate/index.js
+++ quinzena4/objetos/blog/blog-boilerplate/index.js
@@ -1,27 +1,27 @@
 
-arrayTeste = [];
+listaDePosts = [];
 
 function incluirPost() {
-
     const tituloDoPost = document.getElementById('titulo-post').value
     const autorDoPost = document.getElementById('autor-post').value
     const conteudoDoPost = document.getElementById('conteudo-post').value
 
     if (tituloDoPost.length > 0 && autorDoPost.length > 0 && conteudoDoPost.length > 0) {
         postAdicionado = capturarEntradasUsuario(tituloDoPost, autorDoPost, conteudoDoPost) //retorna objeto
-        arrayTeste.push(postAdicionado);
+        listaDePosts.push(postAdicionado);
         limparInputs()
         publicarPost()
     } else {
         alert('Por favor, preencha todos os campos');
         return
     }
-    console.log(arrayTeste)
+    console.log(listaDePosts)
 }
 
 function publicarPost(){
-    let publicarPost = document.getElementById('container-de-posts')
-    publicarPost.innerHTML += `<div class="card-post">${arrayTeste[arrayTeste.length - 1].titulo}<br>${arrayTeste[arrayTeste.length - 1].autor}<br>${arrayTeste[arrayTeste.length - 1].conteudo}</div>`
+    let containerDePosts = document.getElementById('container-de-posts')
+
+    containerDePosts.innerHTML += `<div class="card-post"> <h1>${listaDePosts[listaDePosts.length - 1].titulo}</h1> <h5>${listaDePosts[listaDePosts.length - 1].autor}</h5> <p>${listaDePosts[listaDePosts.length - 1].conteudo}</p> </div>`
 }
 
 function capturarEntradasUsuario(titulo, autor, conteudo) {
